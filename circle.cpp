bf04bafcc2530912d6899cf0207f9d1e8bff7e43
Qerku <102692956+QerkuX@users.noreply.github.com>
2023-03-19 22:13:11 +0100

    CV3
    Program generates circle by transforming angle to a x and y coordinate

--- circle.cpp
+++ circle.cpp
@@ -3,49 +3,54 @@
 using namespace std;
 
 int r;
+int x[361];
+int y[361];
+float rad;
+const float PI = 3.14;
+
+// Checks if current point in the graph is a part of a circle
+bool isValidPlace(int yMap, int xMap){
+    for (int deg = 0; deg <= 360; deg++){
+        if (yMap == y[deg] && xMap == x[deg]){
+            return true;
+        }
+    }
+    
+    return false;
+}
 
 int main(){
-	
-	while (true){
-		cout << "Enter the radius (only odd numbers, because of the rendering system): ";
-		cin >> r;
-		
-		if (r % 2 == 0){
-			r += 1;
-		}
-		
-		cout << endl << endl;
-		
-		int yMin[r*2+1], yMax[r*2+1], fillSpace;
-		
-		for (int i = -r; i <= r; i++){
-			yMax[i+r] = round(sqrt(-pow(i, 2) + pow(r, 2))) + r;
-			yMin[i+r] = r - (yMax[i+r]) + r;
-			
-			if (yMin[i+r] == 0){
-				fillSpace = i+r;
-			}
-		}
-		
-			yMax[r*2] = fillSpace;
-			yMin[r*2] = r - fillSpace + r;
-			
-			yMax[0] = fillSpace;
-			yMin[0] = r - fillSpace + r;
-	 	
-		for (int i = 0; i <= r*2; i++){
-			for (int j = 0; j <= r*2; j++){
-				if (i <= yMax[j] && i >= yMin[j]){
-					cout << "x "; 
-				}
-				else{
-					cout << "  ";
-				}
-			}
-			cout << endl;
-		}
-		
-		cout << endl << endl;
-	}
-	return 0;
+    cout << "radius: ";
+    cin >> r;
+
+    //Goes over every angle from 0 to 360
+    for (int deg = 0; deg <= 360; deg++){
+
+        //Changes degrees to radiants
+        rad = deg * (PI/180);
+
+        //Generates x and y using angle
+        x[deg] = round(sin(rad) * r);
+        y[deg] = round(cos(rad) * r);
+    }
+
+
+    // Goes over every point of the graph
+    for (int yMap = -r; yMap <= r; yMap++){
+        for (int xMap = -r; xMap <= r; xMap++){
+            
+
+            // If point is the part of the graph then draw x,
+            // otherwise leave empty space
+            if (isValidPlace(yMap, xMap)){
+                cout << "x ";
+                continue;
+            }
+        
+            cout << "  ";
+        }
+
+        cout << endl;
+    }
 }
+
